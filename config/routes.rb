ed83fbfe71eb5892751bd2b119b67422de8c6590
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-01-28 13:16:01 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+
+
+  root to: "homes#top"
+  get "about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
